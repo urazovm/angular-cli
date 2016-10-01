c46f202814d8a9c4cdf50c36a95075361c85f3c3
Shyam <shyamindra@gmail.com>
2016-10-02 01:08:09 +0530

    Some changes

---
+++ src/app/fb-sdk/fb-sdk.js
@@ -0,0 +1,5 @@
+// Type definitions for Facebook Javascript SDK
+// Project: https://developers.facebook.com/docs/javascript
+// Definitions by: Joshua Strobl <https://github.com/JoshStrobl>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//# sourceMappingURL=fb-sdk.js.map
